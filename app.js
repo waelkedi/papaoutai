5a98b453c1349e9f6a137a04fbe2bd95503c757c
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-03-05 05:43:37 +0100

    Basically used sessions, POST and GET data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var expressws = require('express-ws')(app);
 var session = require('express-session');
+var bodyParser = require('body-parser');
 var clients = expressws.getWss('/').clients;
 
 app.use(express.static('static'));
@@ -11,21 +12,34 @@ app.use(session({
 	resave: false,
 	saveUninitialized: false,
 }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/tpl', function(req, res, next) {
-	res.render('test', {title: 'hello'});
+app.get('/ask', function(req, res, next) {
+	res.render('ask', {title: 'Demandes', children: [{id: 42, name: 'Toto'}]});
+});
+
+app.post('/ask', function(req, res, next) {
+	//console.log(req.body);
+	for (c in clients) {
+		if (clients[c].upgradeReq.session.name == 'Titi') {
+			var msg = req.body;
+			msg.parent = req.session.name;
+			clients[c].send(JSON.stringify(msg));
+		}
+	}
+	res.end('bouh');
 });
 
 app.get('/setsess', function(req, res, next) {
-	req.session.a = 42;
+	req.session.name = req.query.name;
 	res.send('hello');
 });
 
 app.ws('/', function (ws, req) {
 	ws.on('message', function (msg) {
 		//console.log(msg);
-		console.log(msg+", "+req.session.a);
+		console.log(msg+", "+req.session.name);
 		for (c in clients)
 			clients[c].send(msg);
 	});
